73cddfebf5fe6f84f4a330fbd7146c3b7c414208
Kris <kristhecanadian101@gmail.com>
2022-12-09 18:38:21 -0500

    Updated All models with new data preprocessing

--- datapreprocessing.py
+++ datapreprocessing.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+from collections import Counter
+from imblearn.over_sampling import SMOTE
 from sklearn.model_selection import train_test_split
 import re
 from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 from keras.preprocessing.text import Tokenizer
 from keras.utils import pad_sequences
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def get_cleaned_data():
@@ -83,10 +85,8 @@ def get_cleaned_data():
   # split our data from training & testing
   X_train, X_test, y_train, y_test = train_test_split(df_new.Message, df_new.Label, test_size=0.2, random_state=0)
 
-  #   sm = SMOTE()
-  #   X_train, y_train = sm.fit_resample(X_train.values.ravel(), y_train)
-  #   # print value count
-  #   print(df[X_train].value_counts())
+  print(f'X_train: {X_train.shape}')
+
 
   # tokenize our data
   vocab_size = 10000  # only consider this many words
@@ -101,5 +101,35 @@ def get_cleaned_data():
 
   X_train = pad_sequences(X_train, maxlen=sentence_len)
   X_test = pad_sequences(X_test, maxlen=sentence_len)
+
+
+
+  # Create a SMOTE object
+  smote = SMOTE(random_state=42)
+
+  # Transform the data using the SMOTE object
+  X_train, y_train = smote.fit_resample(X_train, y_train)
+
+  # Count the number of spam and ham samples
+  # Import the seaborn and matplotlib.pyplot libraries
+
+  # Count the number of spam and ham samples
+  class_counts = Counter(y_train)
+
+  # Create a bar plot of the class counts
+  ax = sns.barplot(x=list(class_counts.keys()), y=list(class_counts.values()), color="blue")
+
+  # Set the x-axis labels
+  ax.set_xticklabels(["Spam", "Ham"])
+
+  # Set the y-axis label
+  ax.set_ylabel("Count")
+
+  # Set the plot title
+  ax.set_title("Spam vs. Ham Counts")
+
+  # Show the plot
+  plt.show()
+
 
   return X_train, X_test, y_train, y_test, vocab_size, sentence_len
